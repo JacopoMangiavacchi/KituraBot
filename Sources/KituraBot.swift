dd74cf946cc308de3e1e2b537a7462bc51d77a50
Jacopo Mangiavacchi <jacopo.mangiavacchi@uk.ibm.com>
2016-10-11 11:16:32 +0100

    v0.2.1

--- Sources/KituraBot.swift
+++ Sources/KituraBot.swift
@@ -12,17 +12,85 @@ import SwiftyJSON
 import LoggerAPI
 
 
+public typealias KituraBotContext = [String: Any]
+
+//KituraBot User structure
+//A user id is unique per channel
+public struct KituraBotUser {
+    let userId: String
+    let channel: String
+    
+    public init(userId: String, channel: String) {
+        self.userId = userId
+        self.channel = channel
+    }
+}
+
+
+//KituraBot Message Type enumn
+public enum KituraBotMessageType : Int {
+    case request
+    case response
+}
+
+//KituraBot Message structure
+public struct KituraBotMessage {
+    let messageId: String
+    let timestamp: Date
+    let messageType: KituraBotMessageType
+    let user: KituraBotUser
+    let messageText: String
+    let context: KituraBotContext?
+    
+    //TODO: Add default
+    public init(messageType: KituraBotMessageType, user: KituraBotUser, messageText: String, context: KituraBotContext?) {
+        self.messageId = UUID().uuidString
+        self.timestamp = Date()
+        
+        self.messageType = messageType
+        self.user = user
+        self.messageText = messageText
+        self.context = context
+    }
+}
+
+
+//KituraBot Message Store Protocol for KituraBotMessageStore plugins
+public protocol KituraBotMessageStoreProtocol {
+    func addMessage(_ message: KituraBotMessage)
+    func getMessage(messageId: String) -> KituraBotMessage?
+    //TODO: Pass KituraBotUser (channel + userid)
+    func getMessageAll(user: KituraBotUser) -> [KituraBotMessage]
+    func getMessageAll(fromMessageId: String, user: KituraBotUser) -> [KituraBotMessage]
+    func getMessageAll(fromDate: String, user: KituraBotUser) -> [KituraBotMessage]
+}
+
+
+//KituraBot Message Response structure
+public struct KituraBotMessageResponse {
+    let messageText: String
+    let context:KituraBotContext?
+    
+    public init(messageText: String, context:KituraBotContext?) {
+        self.messageText = messageText
+        self.context = context
+    }
+}
+
+
 //Return back to the channel the Sync Response Message eventually returned by the caller bot logic
-public typealias BotInternalMessageNotificationHandler = (_ channelName: String, _ senderId: String, _ message: String, _ context: [String: Any]?) -> (message: String, context:[String: Any]?)?
+public typealias BotInternalMessageNotificationHandler = (_ message: KituraBotMessage) -> KituraBotMessageResponse?
 
 public protocol KituraBotProtocol {
     func configure(router: Router, channelName: String, botProtocolMessageNotificationHandler: @escaping BotInternalMessageNotificationHandler)
-    func sendTextMessage(recipientId: String, messageText: String, context: [String: Any]?)
+    func sendMessage(_ message: KituraBotMessage)
 }
 
 public enum KituraBotError: Error {
     case channelAlreadyExist
 }
+
+
 
 // MARK KituraBot
 
@@ -34,8 +102,8 @@ public class KituraBot {
         let channel: KituraBotProtocol
     }
 
-    public typealias SyncNotificationHandler = (_ channelName: String, _ senderId: String, _ message: String, _ context: [String: Any]?) -> (message: String, context: [String: Any]?)?
-    public typealias PushNotificationHandler = (_ channelName: String, _ senderId: String, _ message: String, _ context: [String: Any]?) -> (channelName: String, message: String, context: [String: Any]?)?
+    public typealias SyncNotificationHandler = (_ message: KituraBotMessage) -> KituraBotMessageResponse?
+    public typealias PushNotificationHandler = (_ message: KituraBotMessage) -> (channelName: String, message: KituraBotMessageResponse)?
     
     private let syncBotMessageNotificationHandler: SyncNotificationHandler
     private var pushBotMessageNotificationHandler: PushNotificationHandler?
@@ -44,12 +112,28 @@ public class KituraBot {
     private let router: Router
     private var channelDictionary = [String : KituraChannel]()
     
+    private let messageStore: KituraBotMessageStoreProtocol?
+    private let getToken: String
+    
+    
     /// Initialize a `KituraBot` instance.
     ///
     /// - Parameter router: Passed Kitura Router (to add GET and POST REST API for the webhook URI path.
-    public init(router: Router, botMessageNotificationHandler: @escaping (SyncNotificationHandler)) {
+    public init(router: Router, messageStore: KituraBotMessageStoreProtocol?, getPath: String, getToken: String, botMessageNotificationHandler: @escaping (SyncNotificationHandler)) {
         self.router = router
+        self.messageStore = messageStore
+        self.getToken = getToken
+        
         self.syncBotMessageNotificationHandler = botMessageNotificationHandler
+        
+        //Expose router handler for GET API to be called by client to get messages
+        router.get("\(getPath)/:messageId/token/:tokenId", handler: getMessageHandler)
+        
+        //Expose router handler for getting list of messages
+        //TODO: Add user level authentication
+        router.get("\(getPath)/channel/:channelId/user/:userId/token/:tokenId", handler: getMessageAllHandler)
+        router.get("\(getPath)/channel/:channelId/user/:userId/fromId/:fromId/token/:tokenId", handler: getMessagesFromIdHandler)
+        router.get("\(getPath)/channel/:channelId/user/:userId/fromDate/:fromDate/token/:tokenId", handler: getMessagesFromDateHandler)
     }
     
     public func addChannel(channelName: String, channel: KituraBotProtocol) throws {
@@ -63,8 +147,21 @@ public class KituraBot {
     }
     
     // Return to the caller the optionl Syncronous Response Message returned by the caller bot logic
-    private func internalBotProtocolMessageNotificationHandler(_ channelName: String, _ senderId: String, _ message: String, _ context: [String: Any]?) -> (message: String, context: [String: Any]?)? {
-        return syncBotMessageNotificationHandler(channelName, senderId, message, context)
+    private func internalBotProtocolMessageNotificationHandler(_ message: KituraBotMessage) -> KituraBotMessageResponse? {
+        
+        //Save the message received on Message Store
+        messageStore?.addMessage(message)
+        
+        if let messageToReturn = syncBotMessageNotificationHandler(message) {
+            //Save the message to sand back on Message Store
+            let messageReturned = KituraBotMessage(messageType: .response, user: message.user, messageText: messageToReturn.messageText, context: messageToReturn.context)
+            
+            messageStore?.addMessage(messageReturned)
+            
+            return messageToReturn
+        }
+        
+        return nil
     }
     
     public func exposeAsyncPush(securityToken: String, webHookPath: String = "/BotPushBack", pushNotificationHandler: @escaping (PushNotificationHandler)) {
@@ -102,16 +199,24 @@ public class KituraBot {
                     
                     var context = json["context"].dictionaryObject
                     
+                    let user = KituraBotUser(userId: recipientId, channel: channelName)
+                    let messageToPush = KituraBotMessage(messageType: .response, user: user, messageText: finalMessage, context: context)
+                    
                     //Call pushBotMessageNotificationHandler to verify channel and message
-                    if let (newChannelName, newMessage, newContext) = pushBotMessageNotificationHandler?(channelName, recipientId, messageText, context) {
+                    if let (newChannelName, messageResponse) = pushBotMessageNotificationHandler?(messageToPush) {
                         finalChannelName = newChannelName
-                        finalMessage = newMessage
-                        context = newContext
+                        finalMessage = messageResponse.messageText
+                        context = messageResponse.context
                     }
                     
                     //SEND MESSAGE TO THE channel
                     if let channel = channelDictionary[finalChannelName]?.channel {
-                        channel.sendTextMessage(recipientId: recipientId, messageText: finalMessage, context: context)
+                        //Save the asyn message to sand back on Message Store
+                        let user = KituraBotUser(userId: recipientId, channel: channelName)
+                        let messageToFinallyPush = KituraBotMessage(messageType: .response, user: user, messageText: finalMessage, context: context)
+                        
+                        messageStore?.addMessage(messageToFinallyPush)
+                        channel.sendMessage(messageToFinallyPush)
                     }
                     
                     try response.status(.OK).end()
@@ -136,6 +241,137 @@ public class KituraBot {
             
             try response.status(.badRequest).end()
         }
+    }
+    
+    private func getMessageJSON(_ message: KituraBotMessage) -> JSON {
+        var jsonDictionary:[String : Any] = ["messageText" : message.messageText, "messageId" : message.messageId]  //add date, channel
+        
+        let dateFor = DateFormatter()
+        dateFor.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        jsonDictionary["timestamp"] = dateFor.string(from: message.timestamp)
+
+        switch message.messageType {
+        case .request:
+            jsonDictionary["direction"] = ">"
+        case .response:
+            jsonDictionary["direction"] = "<"
+        }
+        
+        //jsonDictionary["userId"] = message.user.userId
+        //jsonDictionary["channel"] = message.user.channel       
+
+        if let context = message.context {
+            jsonDictionary["context"] = context
+        }
+        
+        return JSON(jsonDictionary)
+    }
+
+
+    /// Exposed API to Receive Message context and details
+    /// Get a single message
+    /// "\(getPath)/:messageId/token/:tokenId"
+    /// i.e. http://localhost:8090/message/123/token/1234
+    private func getMessageHandler(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        guard let token = request.parameters["tokenId"], token == getToken else {
+            Log.debug("Passed GET token do not match")
+            print("Passed GET token do not match")
+            
+            try response.status(.badRequest).end()
+            return
+        }
+        
+        if let messageId = request.parameters["messageId"], let message = messageStore?.getMessage(messageId: messageId) {
+            try response.status(.OK).send(json: getMessageJSON(message)).end()
+            return
+        }
+
+        try response.status(.badRequest).end()
+    }
+
+    
+    
+    /// Exposed API to Receive Message context and details
+    /// Get a single message
+    /// "\(getPath)/channel/:channelId/user/:userId/token/:tokenId"
+    /// i.e. http://localhost:8090/message/channel/channel1/user/123/token/1234
+    private func getMessageAllHandler(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        guard let token = request.parameters["tokenId"], token == getToken else {
+            Log.debug("Passed GET token do not match")
+            print("Passed GET token do not match")
+            
+            try response.status(.badRequest).end()
+            return
+        }
+        
+        if let userId = request.parameters["userId"], let channelName = request.parameters["channelId"] {
+            let user = KituraBotUser(userId: userId, channel: channelName)
+            
+            if let messageArray: [KituraBotMessage] = messageStore?.getMessageAll(user: user) {
+                let messageJsonArray = messageArray.map { getMessageJSON($0) }
+                
+                try response.status(.OK).send(json: JSON(messageJsonArray)).end()
+                return
+            }
+        }
+        
+        try response.status(.badRequest).end()
+    }
+    
+    
+    /// Exposed API to Receive Message context and details
+    /// Get all messages from a particualr messageId
+    /// "\(getPath)/channel/:channelId/user/:userId/fromId/:fromId/token/:tokenId"
+    /// i.e. http://localhost:8090/message/channel/channel1/user/123/fromId/123/token/1234
+    private func getMessagesFromIdHandler(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        guard let token = request.parameters["tokenId"], token == getToken else {
+            Log.debug("Passed GET token do not match")
+            print("Passed GET token do not match")
+            
+            try response.status(.badRequest).end()
+            return
+        }
+        
+        if let userId = request.parameters["userId"], let channelName = request.parameters["channelId"] {
+            let user = KituraBotUser(userId: userId, channel: channelName)
+            
+            if let fromId = request.parameters["fromId"], let messageArray: [KituraBotMessage] = messageStore?.getMessageAll(fromMessageId: fromId, user: user) {
+                let messageJsonArray = messageArray.map { getMessageJSON($0) }
+                
+                try response.status(.OK).send(json: JSON(messageJsonArray)).end()
+                return
+            }
+        }
+        
+        try response.status(.badRequest).end()
+    }
+
+    
+    /// Exposed API to Receive Message context and details
+    /// Get all messages from a particualr date
+    /// "\(getPath)/channel/:channelId/user/:userId/fromDate/:fromDate/token/:tokenId"
+    /// http://localhost:8090/message/channel/channel1/user/123/fromDate/123/token/1234
+    private func getMessagesFromDateHandler(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        guard let token = request.parameters["tokenId"], token == getToken else {
+            Log.debug("Passed GET token do not match")
+            print("Passed GET token do not match")
+            
+            try response.status(.badRequest).end()
+            return
+        }
+        
+        if let userId = request.parameters["userId"], let channelName = request.parameters["channelId"] {
+            let user = KituraBotUser(userId: userId, channel: channelName)
+        
+            if let fromDate = request.parameters["fromDate"], let messageArray: [KituraBotMessage] = messageStore?.getMessageAll(fromDate: fromDate, user: user) {
+                let messageJsonArray = messageArray.map { getMessageJSON($0) }
+                
+                try response.status(.OK).send(json: JSON(messageJsonArray)).end()
+                return
+            }
+        }
+        
+        try response.status(.badRequest).end()
     }
 }
 
